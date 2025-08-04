6f339de0d0301828abc42e37ae553691bae7a737
Nayaki Maneeth Reddy <maneethreddy07@gmail.com>
2025-08-04 19:01:12 +0530

    Initial Commit

---
+++ social_Media/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  social_Media
+//
+//  Created by Maneeth Reddy on 04/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ social_Media/social_MediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  social_MediaApp.swift
+//  social_Media
+//
+//  Created by Maneeth Reddy on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct social_MediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
